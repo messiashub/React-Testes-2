c9ff29eb4089fd18163e5d4034eaa60d82775a1c
messiashub <claudineimessias@hotmail.com>
2021-02-04 01:13:56 -0300

    função Map

---
+++ testes/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Map from './componentes/Map'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='Container'>
+        <Map  color="#906f7f"></Map>
+       
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
