5130e79b8cba6d97122164e68a5f35b129b2c7c2
messiashub <claudineimessias@hotmail.com>
2021-02-06 00:34:03 -0300

    Header , Navegação

--- testes/src/componentes/header/Nav.jsx
+++ testes/src/componentes/header/Nav.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-function Nav(){
-    return(
-        null
+function Nav() {
+    return (
+        <div className="nav">
+            <AnchorLink href='#home' offset= '100'>Home</AnchorLink>
+            <AnchorLink href='#foto'>Fotos</AnchorLink>
+            <AnchorLink href='#information  '>Informações</AnchorLink>
+            <AnchorLink href='#contact'>Contato</AnchorLink>
+        </div>
     )
 }
 export default Nav
