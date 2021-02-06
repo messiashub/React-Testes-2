f284acdac90e6e97a9b7efdc61954901f0f8ecea
messiashub <claudineimessias@hotmail.com>
2021-02-06 14:11:00 -0300

    ButtonMob

--- testes/src/componentes/header/ButtonMob.jsx
+++ testes/src/componentes/header/ButtonMob.jsx
@@ -1,9 +1,31 @@
+import React from 'react'
 
 
-function ButtonMob(){
-    return(
-        null
-    )
+
+class ButtonMob extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            visNav:'Flex'
+        }
+
+    }
+    clickMe() {
+        if(this.state.visNav==='flex'){
+            this.setState({visNav:'none'});
+        }else{
+            this.setState({visNav:"flex"})
+        }
+        document.querySelector('.nav').style.display=this.state.visNav;
+    }
+    render() {
+        return (
+            <div className="buttonMob" onClick={this.clickMe.bind(this)}>
+                <img src="/img/botao.png" alt="Botão Mobile" />
+
+            </div>
+        )
+    }
 }
 
 export default ButtonMob
